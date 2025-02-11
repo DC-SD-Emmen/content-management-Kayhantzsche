6febc61d3343fdb891a9e40536649985835ab3e7
Kayhantzsche <144136@student.dcterra.nl>
2025-02-11 13:03:41 +0100

    oefenopdracht 1
    ik heb een form gemaakt en een php script toegevoegd

---
+++ app/oefenopdracht1/Form_handling.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = isset($_POST['username']) ? trim($_POST['username']) : '';
+    $password = isset($_POST['password']) ? trim($_POST['password']) : '';
+
+    if (empty($username) || empty($password)) {
+        echo "Both username and password are required.";
+        exit;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+?>
---
+++ app/oefenopdracht1/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$host = "mysql"; // Le host est le nom du service, présent dans le docker-compose.yml
+$dbname = "my-wonderful-website";
+$charset = "utf8";
+$port = "3306";
+?>
+
+<html>
+<head>
+    <title>Drenthe College docker web server</title>
+</head>
+<body>
+
+<form action="users" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br><br>
+        <button type="submit">Login</button>
+    </form>
+    
+</body>
+</html>
